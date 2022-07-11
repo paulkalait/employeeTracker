8b7ec276f06d86874dd52df837e44a37939516cb
Your Name <kalaitzidispaul@gmail.com>
2022-07-11 13:41:04 -0400

    set up front end

---
+++ employee-tracker-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeList from './components/EmployeeList';
+
+function App() {
+  return (
+    <div className="App">
+     <EmployeeList />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ employee-tracker-frontend/src/components/EmployeeList.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  return (
+    <div className=" row">
+      <h2 className="text-center"> Employees</h2>
+      <div className="row d-flex justify-content-center">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Employee First Name</th>
+              <th>Employee Last Name</th>
+              <th>Employee Email</th>
+              <th>Employee Address</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.id}>
+                {" "}
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.Email}</td>
+                <td>{employee.Address}</td>
+                <td>{employee.salary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
